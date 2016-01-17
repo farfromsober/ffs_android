ab6b38a7b2ea0cb76c0f722c7f3634959574bacc
Miguel Ángel Vélez <mvelezserrano@outlook.es>
2016-01-17 18:38:45 +0100

    Capturamos imagen de la cámara y la almacenamos y mostramos en el ImageButton.

--- app/src/main/java/com/farfromsober/ffs/fragments/NewProductFragment.java
+++ app/src/main/java/com/farfromsober/ffs/fragments/NewProductFragment.java
@@ -3,21 +3,32 @@ package com.farfromsober.ffs.fragments;
 import android.app.Activity;
 import android.app.Fragment;
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.Spinner;
 
 import com.farfromsober.ffs.R;
 import com.farfromsober.ffs.callbacks.ProductsFragmentListener;
 import com.farfromsober.ffs.network.APIManager;
+import com.farfromsober.generalutils.PictureUtils;
 import com.farfromsober.networkviews.callbacks.OnNetworkActivityCallback;
 
+import java.io.File;
 import java.lang.ref.WeakReference;
+import java.sql.SQLOutput;
+import java.util.ArrayList;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -27,9 +38,17 @@ public class NewProductFragment extends Fragment implements AdapterView.OnItemSe
     private APIManager apiManager;
     private WeakReference<OnNetworkActivityCallback> mOnNetworkActivityCallback;
     public ProductsFragmentListener mListener;
+    private File mActualImageFile;
+    private ArrayList<File> mImageFiles;
 
     @Bind(R.id.new_product_category_spinner) Spinner mCategorySpinner;
     @Bind(R.id.new_product_sell_button) Button mSellButton;
+    @Bind(R.id.new_product_image_1) ImageButton mImage1Button;
+    @Bind(R.id.new_product_image_2) ImageButton mImage2Button;
+    @Bind(R.id.new_product_image_3) ImageButton mImage3Button;
+    @Bind(R.id.new_product_image_4) ImageButton mImage4Button;
+
+    private static final int REQUEST_PHOTO = 2;
 
     public NewProductFragment() {
         // Required empty public constructor
@@ -57,6 +76,14 @@ public class NewProductFragment extends Fragment implements AdapterView.OnItemSe
         // Apply the adapter to the spinner
         mCategorySpinner.setAdapter(adapter);
 
+        mImageFiles = new ArrayList<File>();
+
+        setButtonListeners();
+
+        return root;
+    }
+
+    private void setButtonListeners() {
         mSellButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -64,7 +91,38 @@ public class NewProductFragment extends Fragment implements AdapterView.OnItemSe
             }
         });
 
-        return root;
+        mImage1Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                takePhoto(1);
+            }
+        });
+    }
+
+    private void takePhoto(int imageNumber) {
+
+        File externalFilesDir = getActivity().getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        Long tsLong = System.currentTimeMillis()/1000;
+        String ts = tsLong.toString();
+        String imageName = "IMG_" + "user_id" + ts + ".jpg";
+
+        mActualImageFile = new File(externalFilesDir, imageName);
+
+        final Intent captureImage = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        //boolean canTakePhoto = imageFile != null && captureImage.resolveActivity(getActivity().getPackageManager()) != null;
+
+        Uri uri = Uri.fromFile(mActualImageFile);
+        captureImage.putExtra(MediaStore.EXTRA_OUTPUT, uri);
+        startActivityForResult(captureImage, REQUEST_PHOTO);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_PHOTO && resultCode == getActivity().RESULT_OK) {
+            Bitmap bitmap = PictureUtils.getScaledBitmap(mActualImageFile.getPath(), getActivity());
+            mImage1Button.setImageBitmap(bitmap);
+        }
     }
 
     @Override
