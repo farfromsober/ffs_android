5a14673c9cc76b49154f8e877e8714b56c58205a
Miguel Ángel Vélez <mvelezserrano@outlook.es>
2016-01-31 11:18:28 +0100

    Initial Commit

--- app/src/main/java/com/farfromsober/ffs/fragments/ProfileFragment.java
+++ app/src/main/java/com/farfromsober/ffs/fragments/ProfileFragment.java
@@ -1,24 +1,33 @@
 package com.farfromsober.ffs.fragments;
 
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.farfromsober.customviews.CustomFontTextView;
 import com.farfromsober.ffs.R;
 import com.farfromsober.ffs.model.User;
 import com.farfromsober.ffs.utils.SharedPreferencesManager;
+import com.squareup.picasso.Picasso;
 
+import butterknife.Bind;
 import butterknife.ButterKnife;
+import de.hdodenhof.circleimageview.CircleImageView;
 
 import static com.farfromsober.ffs.fragments.FullProfileFragment.ARG_USER;
 
 public class ProfileFragment extends Fragment {
 
-
     private User mUser;
+
+    @Bind(R.id.profile_image) CircleImageView mProfileImage;
+    @Bind(R.id.profile_purchases) CustomFontTextView mProfilePurchases;
+    @Bind(R.id.profile_sales) CustomFontTextView mProfileSales;
+    @Bind(R.id.profile_bought) CustomFontTextView mProfileBought;
 
     public ProfileFragment() {
         // Required empty public constructor
@@ -60,6 +69,17 @@ public class ProfileFragment extends Fragment {
         setHasOptionsMenu(false);
 
         //TODO: pintar los datos del usuario en pantalla: usar @Bind para "findViewById"
+        mProfileSales.append(String.valueOf(mUser.getSales()));
+        //mProfileImage.setImageURI(Uri.parse(mUser.getAvatarURL()));
+
+        if (mUser.getAvatarURL() != null && mUser.getAvatarURL() != "") {
+            Picasso.with(getActivity())
+                    .load(mUser.getAvatarURL())
+                    .placeholder(R.drawable.mavatar_placeholder)
+                    .resize(500, 500)
+                    .centerCrop()
+                    .into(mProfileImage);
+        }
 
         return root;
     }
