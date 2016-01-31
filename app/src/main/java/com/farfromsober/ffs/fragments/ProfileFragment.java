23a09f9cabc87dd7a337cabd2cf199d39a92438a
Miguel Ángel Vélez <mvelezserrano@outlook.es>
2016-01-31 18:12:27 +0100

    Profile fragment with: user location map, user avatar, user name + lastname, user city + state, user transactions info (sales, sold, bought)

--- app/src/main/java/com/farfromsober/ffs/fragments/ProfileFragment.java
+++ app/src/main/java/com/farfromsober/ffs/fragments/ProfileFragment.java
@@ -1,18 +1,31 @@
 package com.farfromsober.ffs.fragments;
 
 
+import android.annotation.TargetApi;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.farfromsober.customviews.CustomFontTextView;
 import com.farfromsober.ffs.R;
 import com.farfromsober.ffs.model.User;
+import com.farfromsober.ffs.network.APIManager;
+import com.farfromsober.ffs.utils.MapUtils;
 import com.farfromsober.ffs.utils.SharedPreferencesManager;
+import com.farfromsober.network.callbacks.OnDataParsedCallback;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -20,14 +33,21 @@ import de.hdodenhof.circleimageview.CircleImageView;
 
 import static com.farfromsober.ffs.fragments.FullProfileFragment.ARG_USER;
 
-public class ProfileFragment extends Fragment {
+public class ProfileFragment extends Fragment implements OnDataParsedCallback<Object> {
 
     private User mUser;
+    protected APIManager apiManager;
+    private String apiCall;
+
+    private SupportMapFragment mMapFragment;
+    private GoogleMap map;
 
     @Bind(R.id.profile_image) CircleImageView mProfileImage;
-    @Bind(R.id.profile_purchases) CustomFontTextView mProfilePurchases;
-    @Bind(R.id.profile_sales) CustomFontTextView mProfileSales;
-    @Bind(R.id.profile_bought) CustomFontTextView mProfileBought;
+    @Bind(R.id.profile_selling_number) CustomFontTextView mProfileSelling;
+    @Bind(R.id.profile_sales_number) CustomFontTextView mProfileSales;
+    @Bind(R.id.profile_bought_number) CustomFontTextView mProfileBought;
+    @Bind(R.id.profile_name) CustomFontTextView mProfileName;
+    @Bind(R.id.profile_location) CustomFontTextView mProfileLocation;
 
     public ProfileFragment() {
         // Required empty public constructor
@@ -69,8 +89,6 @@ public class ProfileFragment extends Fragment {
         setHasOptionsMenu(false);
 
         //TODO: pintar los datos del usuario en pantalla: usar @Bind para "findViewById"
-        mProfileSales.append(String.valueOf(mUser.getSales()));
-        //mProfileImage.setImageURI(Uri.parse(mUser.getAvatarURL()));
 
         if (mUser.getAvatarURL() != null && mUser.getAvatarURL() != "") {
             Picasso.with(getActivity())
@@ -81,7 +99,79 @@ public class ProfileFragment extends Fragment {
                     .into(mProfileImage);
         }
 
+        mProfileName.setText(getString(R.string.profile_fragment_user_name, mUser.getFirstName(), mUser.getLastName()));
+        mProfileLocation.setText(getString(R.string.profile_fragment_user_location, mUser.getCity(), mUser.getState()));
+
+        apiManager = new APIManager(getActivity());
+        apiCall = "Selling";
+        apiManager.userSellingProducts(mUser, this);
+        configureMap();
+
         return root;
     }
 
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
+    private void configureMap() {
+
+        mMapFragment =   ((SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.profile_map));
+
+        if (mMapFragment != null) {
+
+            map = mMapFragment.getMap();
+
+            User user = SharedPreferencesManager.getPrefUserData(getActivity());
+            LatLng center;
+            if (user.getLatitude() == "" || user.getLongitude() == "") {
+                center = MapUtils.getLocationFromAddress(getActivity(), String.format("%s, %s", user.getCity(), user.getState()));
+            } else {
+                center = new LatLng(Double.parseDouble(user.getLatitude()), Double.parseDouble(user.getLongitude()));
+            }
+            MapUtils.centerMap(map, center.latitude, center.longitude, 12);
+            // create marker
+            MarkerOptions marker = new MarkerOptions().position(center);
+            // adding marker
+            map.addMarker(marker);
+        }
+        if (map == null) {
+            Toast.makeText(getActivity(), "Map died", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public void getNextApiCall (ArrayList<Object> data) {
+        switch (apiCall) {
+            case "Selling":
+                if (data != null) {
+                    mProfileSelling.setText(String.valueOf(data.size()));
+                }
+                apiManager.userSoldProducts(mUser, this);
+                apiCall = "Sold";
+                break;
+            case "Sold":
+                if (data != null) {
+                    mProfileSales.setText(String.valueOf(data.size()));
+                }
+                apiManager.userBoughtProducts(mUser, this);
+                apiCall = "Bought";
+                break;
+            case "Bought":
+                if (data != null) {
+                    mProfileBought.setText(String.valueOf(data.size()));
+                }
+                apiCall = "Selling";
+                break;
+        }
+    }
+
+    @Override
+    public void onDataArrayParsed(int responseCode, ArrayList<Object> data) {
+        getNextApiCall(data);
+    }
+
+    @Override
+    public void onDataObjectParsed(int responseCode, Object data) {
+    }
+
+    @Override
+    public void onExceptionReceived(Exception e) {
+    }
 }
